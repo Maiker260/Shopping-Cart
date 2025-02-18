8d80c3974ee91d058d59056cf69a9bbe63f47d21
Maiker260 <gutierrezmaiker@gmail.com>
2025-02-17 21:37:21 -0600

    Style New Products section

--- src/layouts/Navbar/Navbar.jsx
+++ src/layouts/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import NavStyles from "./NavBar.module.css"
-import utilStyles from "../../styles/utilities.module.css"
+import navStyles from "./NavBar.module.css";
+import utilStyles from "../../styles/utilities.module.css";
 import { Link } from "react-router-dom";
 import HomeIcon from "../../components/Icons/HomeIcon";
 import CartIcon from "../../components/Icons/CartIcon";
@@ -8,36 +8,36 @@ import SearchIcon from "../../components/Icons/SearchIcon";
 
 function NavBar({}) {
     return (
-        <nav className={`${NavStyles.navbar} ${utilStyles.flexColumnSpaceAround}`}>
-            <section className={`${utilStyles.flexColumn} ${NavStyles.logoContainer}`}>
-                <Link to="/" className={`${utilStyles.flexCenter} ${NavStyles.list} ${NavStyles.mainTitle}`}>
+        <nav className={`${navStyles.navbar} ${utilStyles.flexColumnSpaceAround}`}>
+            <section className={`${utilStyles.flexColumn} ${navStyles.logoContainer}`}>
+                <Link to="/" className={`${utilStyles.flexCenter} ${navStyles.list} ${navStyles.mainTitle}`}>
                     <h1>MyStore</h1>
                     <img src="/images/ShoppingLogo.webp" alt="StoreLogo" width="80px"/>
                 </Link>
-                <ul className={`${NavStyles.listContainer} ${utilStyles.flexColumn}`}>
-                    <Link to="/" className={`${utilStyles.flexCenter} ${NavStyles.list}`}>
-                        <HomeIcon className={NavStyles.iconList}/>
+                <ul className={`${navStyles.listContainer} ${utilStyles.flexColumn}`}>
+                    <Link to="/" className={`${utilStyles.flexCenter} ${navStyles.list}`}>
+                        <HomeIcon className={navStyles.iconList}/>
                         <li>Home</li>
                     </Link>
-                    <Link to="CategoriesPage" className={`${utilStyles.flexCenter} ${NavStyles.list}`}>
-                        <CategoriesIcon className={NavStyles.iconList}/>
+                    <Link to="CategoriesPage" className={`${utilStyles.flexCenter} ${navStyles.list}`}>
+                        <CategoriesIcon className={navStyles.iconList}/>
                         <li>Categories</li>
                     </Link>
-                    <Link to="SearchPage" className={`${utilStyles.flexCenter} ${NavStyles.list}`}>
-                        <SearchIcon className={NavStyles.iconList}/>
+                    <Link to="SearchPage" className={`${utilStyles.flexCenter} ${navStyles.list}`}>
+                        <SearchIcon className={navStyles.iconList}/>
                         <li>Search</li>
                     </Link>
-                    <Link to="Cart" className={`${utilStyles.flexCenter} ${NavStyles.list}`}>
-                        <CartIcon className={NavStyles.iconList}/>
+                    <Link to="Cart" className={`${utilStyles.flexCenter} ${navStyles.list}`}>
+                        <CartIcon className={navStyles.iconList}/>
                         <li>Cart</li>
                     </Link>
                 </ul>
             </section>
-            <ul className={`${NavStyles.listContainer} ${utilStyles.flexColumn}`}>
-                <Link to="AboutUs" className={`${utilStyles.flexCenter} ${NavStyles.list}`}>
+            <ul className={`${navStyles.listContainer} ${utilStyles.flexColumn}`}>
+                <Link to="AboutUs" className={`${utilStyles.flexCenter} ${navStyles.list}`}>
                     <li>About Us</li>
                 </Link>
-                <Link to="ContactUs" className={`${utilStyles.flexCenter} ${NavStyles.list}`}>
+                <Link to="ContactUs" className={`${utilStyles.flexCenter} ${navStyles.list}`}>
                     <li>Contact Us</li>
                 </Link>
             </ul>
