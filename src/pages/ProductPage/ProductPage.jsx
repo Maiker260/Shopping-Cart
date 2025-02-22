acd5e1272c16404bcb7c2d9ac052108c8db5e34a
Maiker260 <gutierrezmaiker@gmail.com>
2025-02-22 16:28:44 -0600

    Add product page

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,9 +1,16 @@
 import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { ProductsContext } from "../../context/ProductsContext";
 
 export default function ProductPage() {
     const { id } = useParams();
-
+    const { productsData } = useContext(ProductsContext)
+    const product = productsData.find((item) => item && item.id === Number(id))
+console.log(product)
     return (
-        <h1>ProductPage{id}</h1>
+        <div>
+            <h1>ProductPage{id}</h1>
+            {/* <p>{product.title}</p> */}
+        </div>
     );
 }
